3ebab9ea197710af89c2ee0c82d7da20c3d9ebb3
D0KKAEl3I <dldudtjr850@gmail.com>
2020-11-04 22:37:54 +0900

    오프닝 생성

---
+++ js/main.js
@@ -0,0 +1,42 @@
+window.onload = function(){
+    $('html,body').animate({ scrollTop: 0 })
+    startApp();
+}
+
+var openingments = [
+    {ment : "서울"},
+    {ment : "디지텍"},
+    {ment : "카지노에"},
+    {ment : "오신 것을"},
+    {ment : "환영합니다"},
+    {ment : "자"},
+    {ment : "이쪽으로 오시죠"},
+]
+
+function startApp(){
+    //여기서 작업할것
+    opening();
+
+}
+
+function opening(){ 
+    var mentCount = 0;
+    var saying = setInterval(() => {
+        if(mentCount+1 == openingments.length){clearInterval(saying)}
+        if(openingments[mentCount].ment=="환영합니다"){
+            clearInterval(saying); 
+            saying=setInterval(() => {
+                if(mentCount+1 == openingments.length){clearInterval(saying)}
+                $('header').empty()
+                $('header').append(`<p>${openingments[mentCount].ment}</p>`)
+            mentCount++  
+            }, 1000);
+        }
+        $('header').empty()
+        $('header').append(`<p>${openingments[mentCount].ment}</p>`)
+    mentCount++  
+    }, 700);
+    setTimeout(() => {
+        $('html,body').animate({ scrollTop: '1080px' }, 1000)
+    }, 6300);
+}
